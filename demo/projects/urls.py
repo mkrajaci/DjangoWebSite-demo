78f6e7bd31ba6a0e1eeda9702910da45cd90246c
Mario <krajacic.mario@gmail.com>
2022-02-08 17:04:51 +0100

    Second commit

---
+++ demo/projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.projects, name='projects'),
+    path('project/<str:pk>/', views.project, name='project'),
+    path('create-project/', views.create_project, name='create-project'),
+    path('update-project/<str:pk>/', views.update_project, name='update-project'),
+]
---
+++ demo/projects/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+
+def projects(request):
+    projectss = Project.objects.all()
+    context = {'projects': projectss}
+    return render(request=request, template_name='projects/projects.html', context=context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    context = {'project': projectObj}
+    return render(request=request, template_name='projects/single-project.html', context=context)
+
+
+def create_project(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request=request, template_name='projects/project_form.html', context=context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request=request, template_name='projects/project_form.html', context=context)
